8d00c9a5c7ffafef7a317a1e7adba9d17d67be3e
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-29 16:26:14 -0500

    parse and sort issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   """
   
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -23,9 +25,34 @@ defmodule Issues.CLI do
                                aliases: [h: :help])
     case parse do
       {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, count}
+      {_, [user, project, count], _} ->
+        {user, project, String.to_integer(count)}
       {_, [user, project], _} -> {user, project, @default_count}
       _ -> :help
     end
+  end
+  
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_ascending_order
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              &(Map.get(&1, "created_at") <= Map.get(&2, "created_at"))
   end
 end
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,24 @@
-defmodule GithubIssues do
+defmodule Issues.GithubIssues do
   @moduledoc false
+
+  @user_agent [{"User-agent", "Elixir qiao.zhang.2015@gmail.com"}]
+  @github_url Application.get_env(:issues, :github_url)
   
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Poison.parser.parse!(body)}
+  end
+  def handle_response({_, %{body: body}}) do
+    {:error, Poison.parser.parse!(body)}
+  end
+
 end
