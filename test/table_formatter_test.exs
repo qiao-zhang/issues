531b780db0ab4584b6889a155acd2bcdc6cafa80
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-29 18:58:00 -0500

    test and imp for generating sample table

---
+++ test/table_formatter_test.exs
@@ -0,0 +1,30 @@
+defmodule TableFormatterTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+#  alias Issues.TableFormatter, as: TF
+
+  defp simple_test_data!(rows\\2, widths\\[2, 3, 1]) when rows >= 1 do
+    unless Enum.all?(widths, &(&1 >= 1)) do
+      raise ArgumentError, message: "All widths should be greater than 0"
+    end
+    row = for width <- widths, do: String.duplicate("x", width)
+    List.duplicate(row, rows)
+  end
+
+  test "simple_test_data" do
+  assert simple_test_data! ==
+    [
+      ["xx", "xxx", "x"],
+      ["xx", "xxx", "x"],
+    ]
+  end
+
+#  test "split_into_columns" do
+#    table = simple_test_data()
+#    columns = split_into_columns(table)
+#    assert length(columns) = length(widths)
+#    assert List.first(columns) == ["xx", "xx", "xx"]
+#    assert List.last(columns) == ["x", "x", "x"]
+#  end
+end
