f42c78645a219f4f2f04119c162a2bb9294c7cac
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-29 22:46:00 -0500

    impl print_table

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,5 +1,5 @@
 defmodule Issues.TableFormatter do
-  import Enum, only: [ zip: 2, map: 2, max: 1, map_join: 3 ]
+  import Enum, only: [ zip: 2, map: 2, max: 1, map_join: 3, each: 2 ]
   @moduledoc false
   def split_into_columns(rows, headers) do
     for header <- headers do
@@ -21,8 +21,30 @@ defmodule Issues.TableFormatter do
     map_join(widths, " | ", &("~-#{&1}s")) <> "~n"
   end
 
-  def put_one_row(columns, format) do
-    :io.format(format, map(columns, &to_string/1))
+  def put_one_row(row, format) do
+    :io.format(format, row)
+  end
+
+  def put_headers(headers, format) do
+    headers
+    |> map(&to_string/1)
+    |> put_one_row(format)
+  end
+
+  def put_body(columns, format) do
+    columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&put_one_row(&1, format))
+  end
+
+  def print_table(rows, headers) do
+    columns = split_into_columns(rows, headers)
+    widths = widths_of(columns, headers)
+    format = format_for(widths)
+    put_headers(headers, format)
+    put_separator(widths)
+    put_body(columns, format)
   end
 
   defp printable(str) when is_binary(str), do: str
