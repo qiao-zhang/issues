feeb5ddb5a2e4bb2361bd57cbfe2f2d3e6f6edf0
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-29 20:17:33 -0500

    impl widths_of

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,4 +1,5 @@
 defmodule Issues.TableFormatter do
+  import Enum, only: [ zip: 2, map: 2, max: 1 ]
   @moduledoc false
   def split_into_columns(rows, headers) do
     for header <- headers do
@@ -6,6 +7,13 @@ defmodule Issues.TableFormatter do
     end
   end
 
+  def widths_of(columns, headers) do
+    for {c, h} <- zip(columns, headers) do
+      [to_string(h) | c] |> map(&String.length/1) |> max
+    end
+  end
+
   defp printable(str) when is_binary(str), do: str
   defp printable(str), do: to_string(str)
+
 end
