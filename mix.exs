aab701daa8ffdcdafe02a7ac42a95e3c58d4c363
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-30 10:23:27 -0500

    finish issues

--- mix.exs
+++ mix.exs
@@ -3,7 +3,10 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config,
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/qiao-zhang/issues",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -28,8 +31,16 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2",
+      {:httpoison, "~> 0.9"},
+      {:poison, "~> 2.2"},
+      {:ex_doc, "~> 0.12"},
+      {:earmark, "~> 1.0", override: true}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
